3d3b35fd2a3cda917bd8f843428ea4bdcb95d1ab
rinkachi <japonics.email@gmail.com>
2019-01-13 20:01:24 +0100

    Done with MasterMind

--- app/Commands/Task5/MasterMind.php
+++ app/Commands/Task5/MasterMind.php
@@ -40,26 +40,50 @@ class MasterMind extends Command
     {
         try {
 
+            $codesLength = static::$codesLength;
+            $generatedCode = $this->generateCoderCode();
+
+            echo "\n\n";
+            
+            echo "**************************\n";
+            echo "*                        *\n";
+            echo "*     MASTER MIND        *\n";
+            echo "*                        *\n";
+            echo "**************************\n";
+
+            echo "\n\n";
             $tries = readline("Enter tries count: \n");
             $tries= intval(preg_replace("/[^0-9]/", "", $tries));
 
-            echo "Available letters: ";
+            echo "\n\n";
+            echo "=== RULES === \n";
+            echo "1. You can enter only specified letters. Available letters: ";
             foreach (static::$availableLetters as $letter)
             {
                 echo $letter . " ";
             }
+            echo "\n";
 
-            $codesLength = static::$codesLength;
-            $generatedCode = $this->generateCoderCode();
+            echo "2. Code must have {$codesLength} chars.\n";
+            echo "3. If code is not correct you lose you try. \n";
+            echo "\n\n";
 
-            echo "\n Enter you codes {$tries}. Must have {$codesLength} chars: \n";
+            echo "You have {$tries} tries. Good luck!!!\n";
+            echo "START!";
+            echo "\n";
 
             for ($index = 0; $index < $tries; $index++)
             {
                 $number = $index + 1;
+                echo "\n\n";
                 echo "TRY {$number} \n";
                 $enteredMessage = readline("Enter code {$number}:");
                 $enteredMessage = preg_replace("/[^A-Z]/", "", strtoupper($enteredMessage));
+
+                if (!$this->validateEnteredText($enteredMessage)) {
+                    continue;
+                }
+
                 $enteredCode = str_split($enteredMessage);
                 $checkedCode = [];
                 $goodAnswer = true;
@@ -69,33 +93,37 @@ class MasterMind extends Command
                     $letter = $enteredCode[$position];
 
                     if ($letter === $generatedCode[$position]) {
-                        $checkedCodes[$index][$position] = static::$goodPlace;
+                        $checkedCode[$position] = static::$goodPlace;
                         continue;
                     }
 
                     if (in_array($letter, $generatedCode)) {
-                        $checkedCodes[$index][$position] = static::$goodLetter;
+                        $checkedCode[$position] = static::$goodLetter;
                         $goodAnswer = false;
                         continue;
                     }
 
-                    $checkedCodes[$index][$position] = static::$wrongGuess;
+                    $checkedCode[$position] = static::$wrongGuess;
                     $goodAnswer = false;
                 }
 
                 if ($goodAnswer === true) {
                     echo "\n\n YOU WON! \n\n";
                     echo "Coder code was: [" . implode("", $generatedCode) . "] \n";
+                    return;
                 }
 
                 foreach ($checkedCode as $status) {
                     if (!is_null($status)) {
                         echo $status;
+                    } else {
+                        echo " ";
                     }
                 }
             }
+            echo "\n\n";
 
-            echo "\n\n YOU LOST! \n\n";
+            echo "YOU LOST! \n\n";
             echo "Coder code was: [" . implode("", $generatedCode) . "] \n";
 
             return;
@@ -108,8 +136,57 @@ class MasterMind extends Command
 
     public function generateCoderCode(): array
     {
-        return ['B', 'F', 'A', 'C'];
+        $letters = static::$availableLetters;
+        $result = [];
+
+        for ($i = 0; $i < static::$codesLength; $i++)
+        {
+            $randomLetterIndex = rand(0, count($letters) - 1);
+            $result[$i] = $letters[$randomLetterIndex];
+            unset($letters[$randomLetterIndex]);
+            $newLetters = [];
+
+            foreach ($letters as $letter) {
+                $newLetters[] = $letter;
+            }
+
+            $letters = $newLetters;
+        }
+
+        return $result;
     }
+
+    public function validateEnteredText($text): bool
+    {
+        $letters = str_split($text);
+        $enteredTextLength = count($letters);
+        $expectedLength = static::$codesLength;
+
+        if ($enteredTextLength !== $expectedLength) {
+            echo "Code length is incorrect. You length is {$enteredTextLength}, it should be {$expectedLength} \n";
+            return false;
+        }
+
+        $usedLetters = [];
+
+        foreach ($letters as $letter)
+        {
+            if (in_array($letter, $usedLetters)) {
+                echo "Letter {$letter} is duplicated!\n";
+                return false;
+            }
+
+            $usedLetters[] = $letter;
+
+            if (!in_array($letter, static::$availableLetters)) {
+                echo "Letter {$letter} cannot be used!\n";
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
     /**
      * Define the command's schedule.
